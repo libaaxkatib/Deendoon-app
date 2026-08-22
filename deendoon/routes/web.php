0583be952c29dff52b3cc0006f35ad9957c56f02
Asad Mohamed <libaaxkatib@gmail.com>
2026-08-22 18:59:14 +0300

    fix(web): redirect root to admin login

--- deendoon/routes/web.php
+++ deendoon/routes/web.php
@@ -17,9 +17,7 @@ use App\Http\Controllers\Admin\AdminTenantController;
 use App\Http\Controllers\LegalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin/login');
 
 /**
  * Public, unauthenticated legal pages (Mobile Play Store Readiness, Fix
